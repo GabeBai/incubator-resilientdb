d0d25a9cd207d44bb289454a091086ae07136198
Gabe Bai <gabbai@ucdavis.edu>
2024-10-07 10:04:13 -0700

    Initial Commit

--- scripts/deploy/performance/transfer.sh
+++ scripts/deploy/performance/transfer.sh
@@ -1,5 +1,5 @@
-inkscape /Users/gab/repo/result2/flamegraph_1.svg --export-type="png" -o /Users/gab/repo/result2/output1.png
-inkscape /Users/gab/repo/result2/flamegraph_2.svg --export-type="png" -o /Users/gab/repo/result2/output2.png
-inkscape /Users/gab/repo/result2/flamegraph_3.svg --export-type="png" -o /Users/gab/repo/result2/output3.png
-inkscape /Users/gab/repo/result2/flamegraph_4.svg --export-type="png" -o /Users/gab/repo/result2/output4.png
-inkscape /Users/gab/repo/result2/flamegraph_5.svg --export-type="png" -o /Users/gab/repo/result2/output5.png
+inkscape /Users/gab/repo/result1/flamegraph_1.svg --export-type="png" -o /Users/gab/repo/result1/output1.png
+inkscape /Users/gab/repo/result1/flamegraph_2.svg --export-type="png" -o /Users/gab/repo/result1/output2.png
+inkscape /Users/gab/repo/result1/flamegraph_3.svg --export-type="png" -o /Users/gab/repo/result1/output3.png
+inkscape /Users/gab/repo/result1/flamegraph_4.svg --export-type="png" -o /Users/gab/repo/result1/output4.png
+inkscape /Users/gab/repo/result1/flamegraph_5.svg --export-type="png" -o /Users/gab/repo/result1/output5.png
